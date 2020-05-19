5943b6e87d80ce34a44a9ab896d05c19fbcf85aa
Mirian Okradze <okradzemirian@gmail.com>
2020-05-19 14:06:47 +0400

    changed hero's paragraph again

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -7,7 +7,7 @@ const Hero = () => (
             <h1 className='hero__heading'>
                 I'm <span>Mirian Okradze</span>
             </h1>
-            <p className='hero__paragraph'>I build things for the web</p>
+            <p className='hero__paragraph'>I build performant web apps</p>
             <a className='button' href='#projects'>
                 See My Work
             </a>
