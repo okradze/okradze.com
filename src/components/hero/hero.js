b3c15ceebc4102e6e99d4df7fdbf8a1119eee2eb
Mirian Okradze <okradzemirian@gmail.com>
2022-02-05 18:11:39 +0400

    chore: formatted

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -2,14 +2,14 @@ import React from 'react'
 import styles from './hero.module.scss'
 
 const Hero = () => (
-  <section id="home" className={styles.hero}>
-    <div className="container">
+  <section id='home' className={styles.hero}>
+    <div className='container'>
       <div className={styles.heroContent}>
         <h1 className={styles.heroHeading}>
           I'm <span>Mirian Okradze</span>, a software developer, focused on
           building performant web apps
         </h1>
-        <a className={styles.button} href="#projects">
+        <a className={styles.button} href='#projects'>
           See My Work
         </a>
       </div>
