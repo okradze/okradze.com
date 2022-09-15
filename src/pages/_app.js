d5c1bf38f356237f78fa669ff4818b84ccf642c4
Mirian Okradze <okradzemirian@gmail.com>
2022-09-15 11:38:23 +0400

    feat: add google analytics 4

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import { ThemeProvider } from 'next-themes'
 import Layout from '../components/layout/layout'
 import '../index.scss'
@@ -9,6 +10,21 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <title>Mirian Okradze | A Software Developer</title>
       </Head>
+      <Script
+        strategy='afterInteractive'
+        src='https://www.googletagmanager.com/gtag/js?id=G-6D0PQ7GJ0L'
+      ></Script>
+
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', 'G-6D0PQ7GJ0L');
+         `}
+      </Script>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
